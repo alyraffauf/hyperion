b4bdaad2a30c7df3c34a380d7e6e6976e83cbaaf
Aly Raffauf <aly@aly.codes>
2025-12-16 14:18:01 -0500

    build: add fix for nix

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,13 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y nix
+
+# wonky patch for nix on fedora >=42
+mkdir /nix
+
+# wonky patch for nix on fedora >=42
+mkdir /nix
 
 # Use a COPR Example:
 #
