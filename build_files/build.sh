59dd71f0a650ea26d8e5282bb9a89111b142b3f2
Aly Raffauf <aly@aly.codes>
2025-12-17 11:09:17 -0500

    split into build scripts

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,22 +2,9 @@
 
 set -ouex pipefail
 
-### Remove packages
-# Remove Aurora-specific KCM, not suitable for our purposes.
-dnf5 remove -y kcm_ublue
+/ctx/base/01-packages.sh
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# Install necessary modules and plugins for pipewire filter-chains.
-dnf5 install -y lsp-plugins-lv2 lv2-bankstown pipewire-module-filter-chain-lv2
-
-# wonky patch for nix on fedora >=42
-mkdir /nix
+/ctx/base/02-nix.sh
 
 # Use a COPR Example:
 #
